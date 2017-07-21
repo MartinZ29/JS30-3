f985afe547dff91051b14406f93df9200fd0503a
martinz <martinzhangyx@gmail.com>
2017-07-21 12:42:25 -0700

    set new value

--- js30-3.js
+++ js30-3.js
@@ -4,6 +4,7 @@ function handleUpdate(){
     console.log(this.value);
     const suffix = this.dataset.sizing || '';
     console.log(suffix);
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix)
 }
 
 inputs.forEach(input => input.addEventListener('change',handleUpdate));
